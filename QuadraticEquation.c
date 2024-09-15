9b84706a519be97f6b58a8f75491d018b129f675
ARSY8 <arsenijudincev30350@gmail.com>
2024-09-16 00:25:43 +0700

    Add files via upload

--- QuadraticEquation.c
+++ QuadraticEquation.c
@@ -24,20 +24,38 @@ int main(void) {
 
 	float discriminant = (b * b) - (4 * a * c);
 
+	if (a == 0) {
+		if (b == 0) {
+			if (c == 0) {
+				printf("У уравнения бесконечно много решений!\n");
+				return 0;
+			}
+			else {
+				printf("У уравнения нет решений!\n");
+				return 1;
+			}
+		}
+		else {
+			float root = -(c / b);
+			printf("Действительный корень уравнения: %f\n", root);
+			return 0;
+		}
+	}
 
-
-	if (discriminant > 0){
+	if (discriminant > 0) {
 		float root1 = (-b + sqrt(discriminant)) / (2 * a);
 		float root2 = (-b - sqrt(discriminant)) / (2 * a);
 		printf("Действительные корни уравнения:\n");
 		printf("Первый корень: %f\n", root1);
 		printf("Второй корень: %f\n", root2);
 		return 0;
-	} else if (discriminant == 0) {
+	}
+	else if (discriminant == 0) {
 		float root = -b / (2 * a);
 		printf("Действительный корень уравнения: %f\n", root);
 		return 0;
-	} else {
+	}
+	else {
 		float real_part = -b / (2 * a);
 		float imaginary_part = sqrt(fabs(discriminant)) / (2 * a);
 		printf("Комлексные корни уравнения:\n");
