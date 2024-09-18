8fd31ac9a96d147933505a2d34b3e27ff17ee5be
ARSY8 <arsenijudincev30350@gmail.com>
2024-09-18 13:44:24 +0700

    Add files via upload

---
+++ Integers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	int whole_number;
+	printf("Введите положительное целое число:");
+	if ((scanf_s("%d", &whole_number) != 1) | (whole_number <= 0)){
+		printf("Введено не то, что нужно было.");
+			return 1;
+	}
+
+	int sum = 0;
+	int multiplication = 1;
+	while (whole_number > 0) {
+		sum += whole_number % 10;
+		multiplication *= whole_number % 10;
+			whole_number /= 10;
+	}
+
+	printf("Сумма цифр числа: %d\n", sum);
+	printf("Произведение цифр числа: %d", multiplication);
+	return 0;
+}
