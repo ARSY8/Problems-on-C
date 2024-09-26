7ed8f50ddc3df94b8d4efebdcf703a9cab576e4c
A. Yudintsev <arsenijudincev30350@gmail.com>
2024-09-26 13:33:00 +0700

    Add files via upload

---
+++ DayNumber.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int day_number(int day, int month, int year);
+
+int main(void) {
+	int day, month, year;
+
+	printf("Введите день, месяц, год числами через пробел: \n");
+	char addition;
+
+	if (!scanf_s("%d%d%d%c", &day, &month, &year, &addition, 1) || addition != '\n') {
+		printf("Ошибка: ведите корректные числа.");
+		return 1;
+	}
+	else {
+		if (day_number(day, month, year) != -1) {
+			printf("Номер дня в году: %d", day_number(day, month, year));
+			return 0;
+		}
+		else {
+			printf("Ошибка: ведите корректные числа.");
+			return 1;
+		}
+	}
+}
+
+
+int day_number(int day, int month, int year) {
+
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) { //Високосный год
+		int arr[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		return content(arr, day, month, year);
+	}		
+	else {
+		int arr[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		return content(arr, day, month, year);
+	}
+}
+
+int content(int arr[], int day, int month, int year) {
+	int day_sum = day;
+	if ((month >= 1 && month <= 12) && (day <= arr[month - 1] && day > 0)) {
+		for (int i = month - 1; i >= 1; i--) {
+			day_sum += arr[i - 1];
+		}
+		return day_sum;
+	}
+	else {
+		return -1;
+	}
+}
