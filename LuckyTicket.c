51ef0cd299d877f30948439dd1cb975ac56ba4a7
A. Yudintsev <arsenijudincev30350@gmail.com>
2024-10-10 15:51:35 +0700

    Update LuckyTicket.c

--- LuckyTicket.c
+++ LuckyTicket.c
@@ -3,9 +3,9 @@
 int main(void) {
 
 	int lucky_ticket = 0;
-	int arr[28] = {0};
+	int arr[14] = {0};
 
-	for (int i = 0; i <= 454; ++i) {
+	for (int i = 0; i <= 999; ++i) {
 		int whole_number = i;
 		int sum = 0;
 		while (whole_number >= 1) {
@@ -13,15 +13,19 @@ int main(void) {
 			sum += digit;
 			whole_number /= 10;
 		}
-		arr[sum] += 2;
+		if (sum >= 0 && sum <= 13) {
+			arr[sum] += 1;
+		}
 	}
 
-	for (int i = 0; i <= 27; ++i) {
-		lucky_ticket += arr[i] * arr[i];
+
+
+	for (int i = 0; i <= 13; ++i) {
+		lucky_ticket += arr[i] * arr[i] * 2;
 	}
 
 
-	float percent_of_lucky_tickets = 100.0f * lucky_ticket / 999999;
+	float percent_of_lucky_tickets = 100.0f * lucky_ticket / 1000000;
 
 
 	printf("Колличество счастливых билетов: %d\n", lucky_ticket);
