5684944afb6feda6ef0226b70bf723dfb81b02bb
A. Yudintsev <arsenijudincev30350@gmail.com>
2024-11-14 17:32:00 +0700

    Update guessing_the_number.c

--- guessing_the_number.c
+++ guessing_the_number.c
@@ -3,23 +3,21 @@
 int main(void) {
 	int max_number = 100;
 	int min_number = 1;
-	int mid_number = ((max_number + min_number) / 2);
+	int mid_number;
 	char user_response;
 	printf("Загадайте число от 1 до 100\n");
-
-	while (max_number - min_number > 1) {
+	while (min_number < max_number) {
+		mid_number = ((max_number + min_number + 1) / 2);
 		printf("Ваше число больше или равно %d? (y/n)\n", mid_number);
 		scanf_s(" %c", &user_response, 1);
 
 		if (user_response == 'y') {
 			min_number = mid_number;
-			mid_number = ((max_number + min_number + 1) / 2);
 		}
 		else if (user_response == 'n') {
-			max_number = mid_number;
-			mid_number = ((max_number + min_number) / 2);
+			max_number = mid_number - 1;
 		}
 	}
-	printf("Загаданное число - %d", mid_number);
+	printf("Загаданное число - %d", min_number);
 	return 0;
 }
